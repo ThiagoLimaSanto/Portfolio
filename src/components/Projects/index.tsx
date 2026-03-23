58fdbc7745d9f0a5db2e40ead8a755b61a1fbc63
Thiago Lima <tlima7601@gmail.com>
2026-03-23 16:21:35 -0300

    atualizando projetos

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -11,18 +11,18 @@ export function Projects() {
         <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-8 max-w-300 mx-auto">
           <Cards
             title="Sistema de Food Delivery"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            imgUrl="/images/projeto1.png"
+            description="Projeto fullstack feita a fim de praticar conceitos de React, Tailwind CSS, Node.js e TypeScript."
+            imgUrl="/images/foodCommerce.webp"
           />
           <Cards
             title="Sistema de adoção de Pet"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            imgUrl="/images/projeto2.png"
+            description="Sistema feito para aprender a integrar o react com o node.js."
+            imgUrl="/images/getaPet.png"
           />
           <Cards
-            title="Microsoft"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            imgUrl="/images/projeto3.webp"
+            title="Delivery"
+            description="Projeto frontend feita para aprender conceitos de React e Tailwind CSS, e uso de mensagens pelo whatsapp."
+            imgUrl="/images/delivery.webp"
           />
         </div>
       </section>
